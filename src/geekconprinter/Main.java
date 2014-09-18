b74ae1254fb81ae50685005dffb55307eedaa7af
Idan Bachar <idan.bachar@visualtao.com>
2014-09-18 15:58:55 +0300

    ToolpathRenderer imp

--- src/geekconprinter/Main.java
+++ src/geekconprinter/Main.java
@@ -1,21 +1,61 @@
 package geekconprinter;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.command.BasicCommand;
+import org.newdawn.slick.command.Command;
+import org.newdawn.slick.command.InputProvider;
+import org.newdawn.slick.command.InputProviderListener;
+import org.newdawn.slick.command.KeyControl;
 
-public class Main extends BasicGame
+public class Main extends BasicGame implements InputProviderListener 
 {
+	private ToolpathRenderer toolpath = new ToolpathRenderer();
+	private int currentLayer = 0;
+	private InputProvider provider;
+	private Command switchLayer = new BasicCommand("switchLayer");
+	
 	public Main(String gamename)
 	{
 		super(gamename);
 	}
 
 	@Override
-	public void init(GameContainer gc) throws SlickException {}
+	public void init(GameContainer gc) throws SlickException {
+		provider = new InputProvider(gc.getInput());
+		provider.addListener(this);
+		
+		provider.bindCommand(new KeyControl(Input.KEY_RETURN), switchLayer);
+		
+		int c = 10;
+		int layers = 20;
+		double rad = 20.0;
+		ArrayList<ArrayList<Point2D>> path = new ArrayList<ArrayList<Point2D>>();
+		for (int l = 0; l < layers; ++l)
+		{
+			ArrayList<Point2D> layer = new ArrayList<Point2D>();
+			for (int i = 0; i < c; ++i)
+			{
+				double x = Math.cos(i / (float) (c - 1) * Math.PI * 2 + (double) l * 0.5) * rad;
+				double y = Math.sin(i / (float) (c - 1) * Math.PI * 2 + (double) l * 0.5) * rad;
+				Point2D p = new Point2D();
+				p.x = x;
+				p.y = y;
+				layer.add(p);
+			}
+			path.add(layer);
+		}
+		
+		toolpath.setToolpath(path);
+		
+	}
 
 	@Override
 	public void update(GameContainer gc, int i) throws SlickException {}
@@ -23,7 +63,8 @@ public class Main extends BasicGame
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException
 	{
-		g.drawString("Howdy!", 10, 50);
+		g.translate(gc.getWidth() * 0.5f, gc.getHeight() * 0.5f);
+		toolpath.render(g, currentLayer, 0.0f);
 	}
 
 	public static void main(String[] args)
@@ -33,11 +74,28 @@ public class Main extends BasicGame
 			AppGameContainer appgc;
 			appgc = new AppGameContainer(new Main("Simple Slick Game"));
 			appgc.setDisplayMode(640, 480, false);
+			appgc.setMouseGrabbed(false);
+			appgc.setShowFPS(false);
 			appgc.start();
 		}
 		catch (SlickException ex)
 		{
 			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
 		}
+	}
+
+	@Override
+	public void controlPressed(Command arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void controlReleased(Command arg0) {
+		if (arg0 == switchLayer)
+		{
+			currentLayer = (currentLayer + 1) % toolpath.getNumberOfLayers();
+		}
+		
 	}
 }
---
+++ src/geekconprinter/ToolpathRenderer.java
@@ -0,0 +1,57 @@
+package geekconprinter;
+
+import java.util.ArrayList;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
+
+public class ToolpathRenderer {
+	private ArrayList<ArrayList<Point2D>> toolpath;
+	private float totalLength;
+	private ArrayList<Float> lengths;
+	private Point2D marker = new Point2D();
+	public void setToolpath(ArrayList<ArrayList<Point2D>> toolpath)
+	{
+		this.toolpath = toolpath;
+		totalLength = 0.0f;
+		for (int i = 0; i < toolpath.size(); ++i)
+		{
+			ArrayList<Point2D> path = toolpath.get(i);
+		}
+	}
+	
+	public int getNumberOfLayers()
+	{
+		return toolpath.size();
+	}
+	
+	public float GetLayerLength(int layer)
+	{
+		float l = 0.0f;
+		ArrayList<Point2D> path = toolpath.get(layer);
+		for (int i = 0; i < path.size() - 1; ++i)
+		{
+			Point2D p1 = path.get(i + 0);
+			Point2D p2 = path.get(i + 1);
+			l += Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+		}
+		return l;
+	}
+	
+	public void render(Graphics g, int layer, float t)
+	{
+		g.setColor(new Color(1.0f, 1.0f, 1.0f));
+		g.setLineWidth(2.0f);
+		ArrayList<Point2D> path = toolpath.get(layer);
+		for (int i = 0; i < path.size() - 1; ++i)
+		{
+			Point2D p1 = path.get(i + 0);
+			Point2D p2 = path.get(i + 1);
+			g.drawLine((float) p1.x, (float) p1.y, (float) p2.x, (float) p2.y);
+		}
+		
+		float markerRadius = 5.0f;
+		g.setColor(new Color(1.0f, 1.0f, 0.0f));
+		g.fillOval((float) marker.x - markerRadius, (float) marker.y - markerRadius, (float) markerRadius * 2.0f, (float) markerRadius * 2.0f);
+	}
+}
