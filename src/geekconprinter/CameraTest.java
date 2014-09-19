6f0dcabf23a491fdb344fefb75c9d541cb73e86d
Ben Raziel <ben.raziel@autodesk.com>
2014-09-19 10:30:07 +0300

    replacing javacv with opencv hello world app - captures frame from external webcam WARNING: does not work with the mac's internal camera..

--- src/geekconprinter/CameraTest.java
+++ src/geekconprinter/CameraTest.java
@@ -1,43 +1,50 @@
-/**
- * 
- */
 package geekconprinter;
 
-import org.bytedeco.javacv.CanvasFrame;
-//import org.bytedeco.javacv.FFmpegFrameRecorder;
-import org.bytedeco.javacv.OpenCVFrameGrabber;
-//import org.bytedeco.javacpp.avutil;
-import org.bytedeco.javacpp.opencv_core.IplImage;
+import org.opencv.core.Mat;
+import org.opencv.highgui.Highgui;
+import org.opencv.highgui.VideoCapture;
 
 public class CameraTest {
 
-//    public static final String FILENAME = "output.mp4";
+    public static void main (String args[]){
 
-    public static void main(String[] args) throws Exception {
-        OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
-        grabber.start();
-        IplImage grabbedImage = grabber.grab();
+    System.out.println("Hello, OpenCV");
+    // Load the native library.
+    System.loadLibrary("opencv_java249");
 
-        CanvasFrame canvasFrame = new CanvasFrame("Cam");
-        canvasFrame.setCanvasSize(grabbedImage.width(), grabbedImage.height());
+    VideoCapture camera = new VideoCapture(0);
+    try {
+		Thread.sleep(1000);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+    
+    camera.open(0); //Useless
+    if(!camera.isOpened()){
+        System.out.println("Camera Error");
+    }
+    else{
+        System.out.println("Camera OK?");
+    }
 
-        System.out.println("framerate = " + grabber.getFrameRate());
-        grabber.setFrameRate(grabber.getFrameRate());
-//        FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(FILENAME,  grabber.getImageWidth(),grabber.getImageHeight());
-//
-//        recorder.setVideoCodec(13);
-//        recorder.setFormat("mp4");
-//        recorder.setPixelFormat(avutil.AV_PIX_FMT_UYVY422);
-//        recorder.setFrameRate(30);
-//        recorder.setVideoBitrate(10 * 1024 * 1024);
-//
-//        recorder.start();
-        while (canvasFrame.isVisible() && (grabbedImage = grabber.grab()) != null) {
-            canvasFrame.showImage(grabbedImage);
-//            recorder.record(grabbedImage);
-        }
-//        recorder.stop();
-        grabber.stop();
-        canvasFrame.dispose();
+    Mat frame = new Mat();
+
+    //camera.grab();
+    //System.out.println("Frame Grabbed");
+    //camera.retrieve(frame);
+    //System.out.println("Frame Decoded");
+
+    camera.read(frame);
+    System.out.println("Frame Obtained");
+
+    /* No difference
+    camera.release();
+    */
+
+    System.out.println("Captured Frame Width " + frame.width());
+
+    Highgui.imwrite("camera.jpg", frame);
+    System.out.println("OK");
     }
 }
